3ec79756fee97ab779b25e804b447587984b5f5e
natmark <natmark0918@gmail.com>
2016-10-10 14:29:01 +0900

    Initial Commit

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ app.use(bodyParser.json());
 
 app.listen(process.env.PORT || 3000);
 console.log('Server is online.');
+app.get('/', function(req, res) {
+  res.send('connected');
+  var io = require('socket.io-client');
+  var socket = io('ws://ma2016-hakodate-tabletennis.herokuapp.com/');
+  socket.on('connect', function () {
+   socket.emit("C_to_S_message",{msg:"ok"});
+  });
+
+});
 app.post('/', function(req, res) {
   // リクエストボディを出力
      console.log(req.body);
