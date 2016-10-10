f28e1b53e078c63c50a74f0d2587c3c7859ee16c
natmark <natmark0918@gmail.com>
2016-10-10 13:43:40 +0900

    [update] add parameter

--- app.js
+++ app.js
@@ -13,12 +13,16 @@ app.post('/', function(req, res) {
   // リクエストボディを出力
      console.log(req.body);
      // パラメータ名、nameを出力
-     console.log(req.body.name);
+     console.log(req.body.impact_pos);
+     console.log(req.body.impact_speed);
+     console.log(req.body.impact_spin);
+     console.log(req.body.swing_speed);
+     console.log(req.body.swing_type);
 
          var io = require('socket.io-client');
     var socket = io('ws://ma2016-hakodate-tabletennis.herokuapp.com/');
       socket.on('connect', function () {
-        socket.emit("C_to_S_message",{value:req.body.name});
+        socket.emit("C_to_S_message",{impact_pos:req.body.impact_pos,impact_speed:req.body.impact_speed,impact_spin:req.body.impact_spin,swing_speed:req.body.swing_speed,swing_type:req.body.swing_type});
     });
 
 })
